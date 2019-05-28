f83d2e9d7dbc7e8c7241d2c47f39f046043ebec7
Giacomo Parmigiani <gparmigiani@travix.local>
2019-05-28 15:28:14 +0200

    added tests and assert ignoring special chars

--- XDiff.Test/Tests.fs
+++ XDiff.Test/Tests.fs
@@ -5,9 +5,21 @@ open System.Xml.Linq
 open XDiff
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open FSharp.Data
+open System.Text.RegularExpressions
 
 
 type XmlType = XmlProvider<"sample.xml">
+
+[<AbstractClass; Sealed>]
+type Assert private() =
+    
+    static member RemoveSpecialChars(str) =
+          Regex.Replace(str, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled)
+    
+    static member StringEqualsIgnoreSpecialChars(expected: string, actual: string) =
+        let expectedFormat = Assert.RemoveSpecialChars(expected)
+        let actualFormat = Assert.RemoveSpecialChars(actual)
+        Assert.AreEqual(expectedFormat, actualFormat)
 
 [<TestClass>]
 type XDiffTests () =
@@ -27,5 +39,18 @@ type XDiffTests () =
         let second = XmlType.GetSample()
         let firstBook = second.Books.[0].XElement
         firstBook.SetElementValue(XName.Get "author", "test")
-        let x = XmlDiffUtils.Diff(("first",first.XElement),("second",second.XElement),[])
-        Assert.IsFalse(x |> Seq.isEmpty);
+        let x = XmlDiffUtils.Diff(("first",first.XElement),("second",second.XElement),[]) |> Seq.toArray
+        Assert.IsTrue(x.Length = 1)
+        let expected = """value: {Name = "author";
+        A = Some "Gambardella, Matthew";
+        B = Some "test";}"""
+        let diff = x.[0]
+        Assert.StringEqualsIgnoreSpecialChars(diff, expected);
+
+  
+
+
+
+
+    
+
